11a30f7a7595195256a8bdbb20876af8f800eb51
Uy Nguyen <uynguyen1501@gmail.com>
2026-01-05 21:33:57 +0100

    doc: homepage

--- src/main/java/com/uynguyen/jwt_spring_security/HomeController.java
+++ src/main/java/com/uynguyen/jwt_spring_security/HomeController.java
@@ -1,10 +1,12 @@
 package com.uynguyen.jwt_spring_security;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Home", description = "Homepage for dev environment")
 public class HomeController {
 
     @Value("${spring.application.name}")
