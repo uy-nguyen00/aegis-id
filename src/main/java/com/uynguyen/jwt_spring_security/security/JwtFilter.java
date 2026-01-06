3f92d647cf4c62d96fea2bbe819816a4aa1c79a0
Uy Nguyen <uynguyen1501@gmail.com>
2026-01-06 23:13:26 +0100

    fix: remove NonNull from overriden method

--- src/main/java/com/uynguyen/jwt_spring_security/security/JwtFilter.java
+++ src/main/java/com/uynguyen/jwt_spring_security/security/JwtFilter.java
@@ -7,7 +7,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
-import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -25,9 +24,9 @@ public class JwtFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(
-        @NonNull HttpServletRequest request,
-        @NonNull HttpServletResponse response,
-        @NonNull FilterChain filterChain
+        HttpServletRequest request,
+        HttpServletResponse response,
+        FilterChain filterChain
     ) throws ServletException, IOException {
         // For minor speed boost
         if (request.getServletPath().contains("/api/v1/auth")) {
