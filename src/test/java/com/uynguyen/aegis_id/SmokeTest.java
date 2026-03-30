d37a0f30dab5a05207af4de8e7017988d8fb7ed4
Uy Nguyen <uynguyen1501@gmail.com>
2026-03-30 22:12:59 +0200

    fix(test): Adapt SmokeTest on both dev and prod profiles

--- src/test/java/com/uynguyen/aegis_id/SmokeTest.java
+++ src/test/java/com/uynguyen/aegis_id/SmokeTest.java
@@ -6,19 +6,29 @@ import com.uynguyen.aegis_id.user.UserController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
 
 @SpringBootTest
 class SmokeTest {
 
-    @Autowired
+    @Autowired(required = false)
     private HomeController homeController;
 
     @Autowired
     private UserController userController;
 
+    @Autowired
+    private Environment environment;
+
     @Test
     void contextLoads() {
-        assertThat(homeController).isNotNull();
+        if (environment.acceptsProfiles(Profiles.of("dev"))) {
+            assertThat(homeController).isNotNull();
+        } else {
+            assertThat(homeController).isNull();
+        }
+
         assertThat(userController).isNotNull();
     }
 }
